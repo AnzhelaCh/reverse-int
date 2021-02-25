d6f0bf873a2bf8f466191e1386d2b50afde99cd6
Chernenko Anzhela <chelsea@templatemonster.me>
2021-02-25 19:32:42 +0200

    feat: add fuction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let numbers = String(n);
+    if (numbers[0] === "-") {
+        numbers = numbers.slice(1);
+    }
+    let newNumbers = numbers.split("").reverse().join("");
+    return Number(newNumbers);
 }
